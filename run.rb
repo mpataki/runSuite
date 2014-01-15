3b5fdbd3a8dd1b08922bc574470cd252ad7db81c
mpataki <matpataki@gmail.com>
2014-01-15 00:27:21 -0500

    cleanup if exception raised

--- run.rb
+++ run.rb
@@ -4,32 +4,35 @@ require 'fileutils'
 # Usage: ruby run.rb program_1 program_2 test_suite_directory
 
 # MAIN
-
-prog1 = ARGV.shift
-prog2 = ARGV.shift
-suite = ARGV.shift  
-unless File.exists?(prog1) && File.exists?(prog2) && Dir.exists?(suite)
-  puts "Usage: ruby run.rb program_1 program_2 test_suite_directory"
-  exit
-end
-
-temp_file1, temp_file2 = "", ""
-begin temp_file1 = "#{prog1}_#{rand(1000)}.out" end while File.exists? temp_file1
-begin temp_file2 = "#{prog2}_#{rand(1000)}.out" end while File.exists? temp_file2
-
-Dir.open(suite).each do |file_name|
-  next if ['.', '..', temp_file1, temp_file2].include? file_name
-  
-  params = File.read("#{suite}/#{file_name}")
-
-  system "./#{prog1} #{params} > #{temp_file1}"
-  system "./#{prog2} #{params} > #{temp_file2}"
-
-  if FileUtils::compare_file temp_file1, temp_file2
-    puts "#{file_name} PASSED"
-  else
-    puts "#{file_name} FAILED"
+begin
+  prog1 = ARGV.shift
+  prog2 = ARGV.shift
+  suite = ARGV.shift  
+  unless File.exists?(prog1) && File.exists?(prog2) && Dir.exists?(suite)
+    puts "Usage: ruby run.rb program_1 program_2 test_suite_directory"
+    exit
   end
-end
 
-File.delete temp_file1, temp_file2
+  temp_file1, temp_file2 = "", ""
+  begin temp_file1 = "#{prog1}_#{rand(1000)}.out" end while File.exists? temp_file1
+  begin temp_file2 = "#{prog2}_#{rand(1000)}.out" end while File.exists? temp_file2
+
+  Dir.open(suite).each do |file_name|
+    next if ['.', '..', temp_file1, temp_file2].include? file_name
+    
+    params = File.read("#{suite}/#{file_name}")
+
+    system "./#{prog1} #{params} > #{temp_file1}"
+    system "./#{prog2} #{params} > #{temp_file2}"
+
+    if FileUtils::compare_file temp_file1, temp_file2
+      puts "#{file_name} PASSED"
+    else
+      puts "#{file_name} FAILED"
+    end
+  end
+
+  File.delete temp_file1, temp_file2
+rescue
+  File.delete temp_file1, temp_file2
+end
