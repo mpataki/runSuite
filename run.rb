1dbd65dbd56218d0eed53288741f5cfe83f405af
mpataki <matpataki@gmail.com>
2014-01-16 00:11:54 -0500

    better output + exceptions

--- run.rb
+++ run.rb
@@ -1,17 +1,27 @@
 require 'fileutils'
+require 'rainbow'
 
 # Compare program_1 & program_2 output
 # Usage: ruby run.rb program_1 program_2 test_suite_directory
 
+class UsageException < StandardError
+end
+
+class NoTestFileException < StandardError
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+end
+
 # MAIN
 begin
+  raise UsageException unless ARGV.count == 5
   prog1 = ARGV.shift
   prog2 = ARGV.shift
   suite = ARGV.shift  
-  unless File.exists?(prog1) && File.exists?(prog2) && Dir.exists?(suite)
-    puts "Usage: ruby run.rb program_1 program_2 test_suite_directory"
-    exit
-  end
+  throw UsageException.new unless File.exists?(prog1) && File.exists?(prog2) && Dir.exists?(suite)
 
   temp_file1, temp_file2 = "", ""
   begin temp_file1 = "#{suite}/#{prog1}_#{rand(1000)}.out" end while File.exists? temp_file1
@@ -19,20 +29,26 @@ begin
 
   Dir.open(suite).each do |file_name|
     next if ['.', '..', temp_file1, temp_file2].include? file_name
-    
-    params = File.read("#{suite}/#{file_name}")
+    params = File.read "#{suite}/#{file_name}"
+    raise NoTestFileException.new("Couldn't open #{file_name}") if params.nil?
 
     system "./#{prog1} #{params} > #{temp_file1}"
     system "./#{prog2} #{params} > #{temp_file2}"
 
     if FileUtils::compare_file temp_file1, temp_file2
-      puts "#{file_name} PASSED"
+      puts Rainbow("#{file_name} PASSED").green
     else
-      puts "#{file_name} FAILED"
+      puts Rainbow("#{file_name} FAILED").red
+      puts Rainbow("#{temp_file1} : #{temp_file2}").yellow
+      system "diff #{temp_file1} #{temp_file2}"
     end
   end
 
-  File.delete temp_file1, temp_file2
-rescue
-  File.delete temp_file1, temp_file2
+rescue UsageException => e
+  puts Rainbow("Usage: ruby run.rb program_1 program_2 test_suite_directory").red
+  exit
+rescue NoTestFileException => e
+  puts Rainbow(e.message).red
 end
+
+File.delete temp_file1, temp_file2
